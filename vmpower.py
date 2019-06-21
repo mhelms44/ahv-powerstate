dcef8735d472dfb2b673923d6c6f64f092160f18
Michael Helms <mhelms92@gmail.com>
2019-06-21 18:33:44 -0400

    Logic change

--- vmpower.py
+++ vmpower.py
@@ -8,16 +8,23 @@ args = parser.parse_args()
 logs = str(args.acropolislog.read())
 vmList = str(args.vmlist.read())
 
-uuid = re.findall("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", vmList)
-#vmName = re.sub("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", "", vmList)
-#vmNameList = vmName.split('\n')
+uuid = re.findall( "[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", vmList)
 
-#print(uuid)
+vmName = re.sub("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", "", vmList)
+vmNameList = vmName.split()
+print (uuid)
+print (vmNameList)
+
+for u, v in zip(uuid, vmNameList):
+    print(v + "\t" + u)
+
+
+
+
+'''
 for vmsID in uuid:
     for powerEvents in logs.split("\n"):
         if vmsID in powerEvents:
             grep = re.match(r'^[^:]+:(?P<date>.+?) INFO.+(?P<event>kPower[^\)]+)', powerEvents.strip())        
-            print(grep.group("date") + (" ") + grep.group("event"))    
-
-            
-            
+            print(grep.group("date") + (" ") + grep.group("event"))
+'''
