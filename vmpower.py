f3d4544f54b23a1ae854b632accea213a16e548f
Michael Helms <mhelms92@gmail.com>
2019-06-26 15:58:14 -0400

    oops

--- vmpower.py
+++ vmpower.py
@@ -11,8 +11,6 @@ def main(acroplogs, vmlist):
     uuid = re.findall( "[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", vmList)
     vmName = re.sub("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", "", vmList)
     vmNameList = vmName.split()
-    #print (uuid)
-    #print (vmNameList)
     for u, v in zip(uuid, vmNameList):
         print(v + "\t" + u)
         for powerEvents in logs.split("/n"):
